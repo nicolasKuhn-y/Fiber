2976e07c2749e2fd71e892169bf28f2f4d6e0d76
Nicolas Kuhn <nicolaskuhn02@gmail.com>
2021-06-17 21:07:44 -0300

    Fixes for Firefox layout

--- src/script/index.js
+++ src/script/index.js
@@ -2,5 +2,9 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const navigation = document.querySelector(".navigation");
 
 mobileMenu.addEventListener("click", () => {
-    navigation.classList.toggle("active");
-})
+  navigation.classList.toggle("active");
+});
+
+document.querySelector(".header_btn")
+    .addEventListener("click", () => window.location.replace("./sign-up-page.html")
+    );
